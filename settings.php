da48ffa4a58dfe119cc96ba250be6da01d5479e0
Emir <emiratesbp@gmail.com>
2015-04-28 12:39:44 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa=""; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="bp"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2396497272;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slide"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
